2d2fc8c5f1e7f18e00af444655ac00554e8ee5eb
zhu609 <2487429158@qq.com>
2026-05-13 21:47:08 +0800

    Delete 新建学习requests directory

--- 新建学习requests/01.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = 'http://www.baidu.com/'
-res= requests.get(url)
-
-print(res)
--- 新建学习requests/image.py
+++
@@ -1,8 +0,0 @@
-import requests
-from PIL import Image
-from io import BytesIO
-r = requests.get("https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo_2x.jpg!0x0.webp")
-i = Image.open(BytesIO(r.content))
-fp = open("1.jpg","wb")
-i.save(fp)
-fp.close()
